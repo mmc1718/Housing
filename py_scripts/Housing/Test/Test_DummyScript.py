063370d747cf07e021a4dcf5a9392be40c7f3caf
DESKTOP-A993VA3\verab <vera.brockmeyer@gmail.com>
2022-04-10 21:49:39 +0200

    Add Script template with Wrapper Classes for easier scripting and higher readbitilty of scripts

---
+++ py_scripts/Housing/Test/Test_DummyScript.py
@@ -0,0 +1,72 @@
+import sys
+import os
+sys.path.append(os.getcwd())
+
+
+from seatable_api import Base, context
+import utils.DummyScript as ds
+from utils.DummyScript import HostDatabase as Hosts
+from utils.DummyScript import RefugeeDatabase as Refugees
+
+def initBase():
+    server_url = context.server_url or 'https://cloud.seatable.io'
+    api_token = context.api_token or 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkdGFibGVfdXVpZCI6IjljNDNhNTU4LWIzNmEtNGMyMy1iM2Y0LTQ0MThmMDRkY2QwZSIsImFwcF9uYW1lIjoiNzdXcC5weSIsImV4cCI6MTY0OTYxODQwNn0.CspygVRHeRt2ivLDpCLRdK4wp4e8WlIc2Q3oxCYptzo'
+    base = Base(api_token, server_url)
+    base.auth()
+    return base
+    
+
+if __name__ == "__main__":
+    base = initBase()
+    hosts = Hosts(base)
+    refugees = Refugees(base)
+    
+    print("******** Hosts Test **************")
+
+    hosts.appendRow(hosts.getDefEntry())
+    print("All Rows: \n")
+    print(hosts.getAllRows())
+    print ("\n")
+    print("All Rows of View: \n")
+    print(hosts.getAllRowsOfView("Simple View"))
+    print("\n")
+    genId = "H-000001"
+    rowId = hosts.getRowIdOfGenId(genId)
+    print("Get Row By Gen Id " + genId +": " + rowId)
+    genId = hosts.getGenIdOfRowId(rowId)
+    print("Get Gen Id by Row Id " + rowId +": " + genId)
+    hosts.updateTimeStamp("Date",rowId)
+    print("Diff after update is " + genId + " is: " + str(hosts.getTimeDiff("Date", rowId)))
+    rows_data = (hosts.getAllRowsOfView("Simple View"))
+    hosts.batchUpdate(rows_data)
+    print("All Rows after Batch Update: \n")
+    print(hosts.getAllRows()) 
+    
+    print("******** Refugee Test **************")
+    
+    refugees.appendRow(refugees.getDefEntry())
+    print("All Rows: \n")
+    print(refugees.getAllRows())
+    print ("\n")
+    print("All Rows of View: \n")
+    print(refugees.getAllRowsOfView("Simple View"))
+    print("\n")
+    genId = "R-000002"
+    rowId = refugees.getRowIdOfGenId(genId)
+    print("Get Row By Gen Id " + genId +": " + rowId)
+    genId = refugees.getGenIdOfRowId(rowId)
+    print("Get Gen Id by Row Id " + rowId +": " + genId)
+    refugees.updateTimeStamp("Date",rowId)
+    print("Diff after update is " + genId + " is: " + str(refugees.getTimeDiff("Date", rowId)))
+    rows_data = (refugees.getAllRowsOfView("Simple View"))
+    refugees.batchUpdate(rows_data)
+    print("All Rows after Batch Update: \n")
+    print(refugees.getAllRows()) 
+    
+    
+    
+    
+    
+    
+    
+    
--- py_scripts/Housing/utils/DummyScript.py
+++ py_scripts/Housing/utils/DummyScript.py
@@ -1,63 +1,151 @@
     
+from pickle import NONE
+from xmlrpc.client import MAXINT
 from seatable_api import Base, context
 from seatable_api.date_utils import dateutils
 
+class TableDef:
+    HOST = "Hosts"
+    VOLUNTEERS = "Volunteers"
+    REFUGEES = "Refugees"
 
-_HOST_TABLE = "Hosts"
-_HR_TABLE = "HR"
-_REFUGEE_TABLE = "Refugees"
-
+class ViewDef:
+    DEFAULT = "Default View"
+    SIMPLEMATCHING = "Simple Matching"
 
 class Database:
     def __init__(self, base=Base, tableName=str):
         self._base      = base
         self._tableName = tableName
+        self._defEntry = {}
+        listOfTables = self._base.get_metadata().get('tables')
+        for t in listOfTables:
+            if t['name'] == tableName:
+                self._tableID= t['_id']        
+                self._listOfViews = t['views']
+                break
+
+    def setDefEntry(self, entry):
+        self._defEntry =  entry  
+    def getDefEntry(self):
+        return self._defEntry
     def getAllRows(self):
-        return self._base.list_rows(self._tableName, view_name=None, order_by=None, desc=False, start=None, limit=None) 
+        rows = self._base.list_rows(self._tableName, view_name=None, order_by=None, desc=False, start=None, limit=None) 
+        if len(rows)>0:
+            return rows
+        else:
+            print(print.__name__ + " Table does not exist")
+            return {}
     def getAllRowsOfView(self, viewName):
-        return self._base.list_rows(self._tableName, view_name=viewName, order_by=None, desc=False, start=None, limit=None)
-    def getBase(self):
-        return self._base
+        rows = self._base.list_rows(self._tableName, view_name=viewName, order_by=None, desc=False, start=None, limit=None) 
+        if len(rows)>0:
+            return rows
+        else:
+            print(print.__name__ + " View does not exist") 
+            return {}    
     def appendRow(self, row_data):
         return self._base.append_row(self._tableName, row_data)
-    def getRowByGeneratedID(self, id):
+    def getRowByGenId(self, id):
         rows = self.getAllRows()
         for r in rows:
             if r.get('ID')==id:
                 return r
-    def getTableIdofGeneratedID(self, genID):
-        return self.getRowByGeneratedID(genID).get('row_id')
-    def getRowByTableId(self, id):
-        return self._base.get_row(self._tableName, id)
+        print(print.__name__ + " Gen ID does not exist") 
+        return {}
+    def getRowIdOfGenId(self, genID):
+        row = self.getRowByGenId(genID)
+        if '_id' in row.keys():          
+            return row['_id']
+        else:
+            print(print.__name__ + " Row ID does not exist")  
+            return "ID Not Existing"
+    def getGenIdOfRowId(self, id):
+        row = self.getRowByRowId(id)
+        if 'ID' in row.keys():          
+            return row['ID'] 
+        else:
+            return "ID Not Existing"
+    
+    def getRowByRowId(self, id):
+        row = self._base.get_row(self._tableName, id)
+        if len(row)>0:
+            return row
+        else:
+            print(print.__name__ + " Row does not exist") 
+            return {}
     def batchUpdate(self, rows_data):
-        self._base.batch_update_rows(self._tableName, rows_data)
-    #def updateTimeStamp()
-    #getTimeSinceLatestDate()    
-    #addLinkList
-    #createLinkListAppliedFilter
-    #clearLinkList 
-    #appendLinkList
-    #getContactData
+        if len(rows_data)>0:
+            newStructure = []
+            for r in rows_data:
+                newEntry={'row_id':r['_id']}
+                row = {}
+                for key, value in r.items():
+                    if key.rfind('_') == -1:
+                        row.update({key:value})
+                newEntry.update({'row':row })
+            newStructure.append(newEntry)
+            self._base.batch_update_rows(self._tableName, newStructure)
+        else:
+            print(print.__name__ +" Batch Update got empty row_data")
+    def updateRowCell(self, Id, column, value):
+        row = self._base.get_row(self._tableName,Id)
+        if len(row)>0:
+            row[column] = value
+            self._base.update_row(self._tableName, Id, row) 
+        else:
+            print(print.__name__ + " Row does not exist")           
+    def updateTimeStamp(self, columnName, Id):
+        row = self._base.get_row(self._tableName,Id)
+        if len(row)>0:
+            row[columnName]= dateutils.now()
+            self._base.update_row(self._tableName, Id, row)
+        else:
+            print(print.__name__ + " Row does not exist")
+    def getTimeDiff(self, columnName, Id, unit='H'):
+        row = self._base.get_row(self._tableName,Id)
+        if len(row)>0:
+            dt = dateutils.datediff(row[columnName], dateutils.now(), unit)
+            return dt
+        else: return NONE
     
     
 
-
+class HostDatabase(Database):
+    def __init__(self, base=Base):
+        super().__init__(base, TableDef.HOST)
+        super().setDefEntry({  
+                    "Status":   "Open",           
+                    "Name":     "Johnny Doe",
+                    "Telefon":  "12345678",
+                    "Email":    "johnny@doe.de",
+                    "Date":     dateutils.date(2022, 1, 1),
+                    "Adresse":  "Seaview Road 100",
+                    "City":     "Hintertupfingen",
+                    "Max. Number Guests":  1,
+                    "Duration": "Up to 3 Months",
+                    "Accomodation Type": "Shared Room",
+                    "Welcoming":    ["Dogs", "Cats", "Babys", "Females"]})
+    
+class RefugeeDatabase(Database):
+    def __init__(self, base=Base):
+        super().__init__(base, TableDef.REFUGEES)
+        super().setDefEntry({    
+                    "Status":   "Open",           
+                    "Name":     "Jimmy Doe",
+                    "Telefon":  "12345678",
+                    "Email":    "johnny@doe.de",
+                    "Date":     dateutils.date(2022, 1, 1),
+                    "Adresse":  "Seaview Road 100",
+                    "City":     "Hintertupfingen",
+                    "N People":  1,
+                    "Duration": "Up to 3 Months",
+                    "Accomodation Type": "Shared Room",
+                    "Short Description":  ["Mom with Kids", "Cat/s", "Baby/s"]})
+    
 class HostingBase:
     def __init__(self, base=Base):
         self._base = base
-        self._hosts = Database(base,_HOST_TABLE)
-        self._refugees = Database(base,_REFUGEE_TABLE)
-        self._defHostEntry = {    
-                "Name":     "John Doe",
-                "City":     "Hintertupfingen",
-                "NGuests":  1,
-                "Date":     dateutils.date(1999, 1, 1),
-                "Email":    "john@doe.de",
-                "Phone":    "01234567890"}
-        self._defRefugeeEntry = {    
-                "Name":     "John Doe",
-                "City":     "Hintertupfingen",
-                "NGuests":  1,
-                "Date":     dateutils.date(1999, 1, 1),
-                "Email":    "john@doe.de",
-                "Phone":    "01234567890"}   
+        self._hosts = HostDatabase(base,TableDef.HOST)
+        self._refugees = RefugeeDatabase(base,TableDef.HOST)
+        
+        
